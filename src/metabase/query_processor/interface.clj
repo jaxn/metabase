e02cc36fd15d890789ba18667df20c787ee56a22
Ryan Senior <ryan@metabase.com>
2017-06-02 09:12:08 -0500

    Revert "Add support for enum-like values"
    This reverts commit 5d4fe82374cbd6de3e8c87843f98ef1b58298093.

--- src/metabase/query_processor/interface.clj
+++ src/metabase/query_processor/interface.clj
@@ -210,16 +210,9 @@
 
 (s/defrecord SQLExpression [expr :- su/NonBlankString])
 
-(s/defrecord RemapExpression [column :- FieldPlaceholderOrExpressionRef
-                              mapping :- {s/Int s/Str}])
-
-(s/defrecord FKRemapExpression [column :- FieldPlaceholderOrExpressionRef
-                                foreign-column :- FieldPlaceholderOrExpressionRef
-                                remapping-column :- FieldPlaceholderOrExpressionRef])
-
 (def AnyExpression
   "Schema for all expressions allowed in MBQL"
-  (s/named (s/cond-pre ArithmeticExpression SQLExpression RemapExpression FKRemapExpression)
+  (s/named (s/cond-pre ArithmeticExpression SQLExpression)
            "Valid expression."))
 
 (def AnyFieldOrExpression
