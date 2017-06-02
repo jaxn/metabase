c1f22eca716e4d252e760e93825c7d674b18ff49
Ryan Senior <ryan@metabase.com>
2017-06-02 09:12:06 -0500

    Revert "Switch remap expression maps to string keys"
    This reverts commit 68eb548c58f3427f89eb20aab546fe51e435e40d.

--- src/metabase/query_processor/middleware/expand.clj
+++ src/metabase/query_processor/middleware/expand.clj
@@ -420,11 +420,8 @@
   `COLUMN`. `COLUMN` values are the keys in `MAPPING`, with the value
   found in `MAPPING` as the result of the expression."
   [column :- i/FieldPlaceholderOrExpressionRef
-   mapping :- {s/Str s/Str}]
-  (i/map->RemapExpression {:column column
-                           :mapping (reduce-kv (fn [acc k v]
-                                                 (assoc acc (Integer/parseInt k) v))
-                                               {} mapping)}))
+   mapping :- {s/Int s/Str}]
+  (i/map->RemapExpression {:column column :mapping mapping}))
 
 (s/defn ^:ql fk-remap-expression
   "Expression that uses `COLUMN` and `FOREIGN-COLUMN` to project out a
